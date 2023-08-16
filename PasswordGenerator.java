ef8030c6cf9e27016580d0a9ab2f529a0d974e0d
AKSHAY <130675828+akshaymatere-333@users.noreply.github.com>
2023-08-16 14:55:17 +0530

    Random Password Generator

---
+++ PasswordGenerator.java
@@ -0,0 +1,27 @@
+//Password Generator Using Java
+
+import java.security.SecureRandom;
+
+public class PasswordGenerator {
+
+    private static final String CHARACTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_-+=<>?";
+
+    public static void main(String[] args) {
+        int length = 12;
+        String password = generatePassword(length);
+        System.out.println("Generated Password: " + password);
+    }
+
+    public static String generatePassword(int length) {
+        SecureRandom random = new SecureRandom();
+        StringBuilder sb = new StringBuilder(length);
+
+        for (int i = 0; i < length; i++) {
+            int randomIndex = random.nextInt(CHARACTERS.length());
+            char randomChar = CHARACTERS.charAt(randomIndex);
+            sb.append(randomChar);
+        }
+
+        return sb.toString();
+    }
+}
